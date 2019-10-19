8407fc27fd2f78686db7dfcdf6375eb2ab1bc42c
Brian May <brian@linuxpenguins.xyz>
2019-10-19 13:12:39 +1100

    Include credo

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Robotica.MixProject do
       {:poison, "~> 4.0.1"},
       {:tortoise, "~> 0.9.2"},
       {:dialyxir, "~> 1.0.0-rc.2", only: [:dev], runtime: false},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
       {:lifx, git: "https://github.com/brianmay/lifx.git"},
       {:yaml_elixir, "~> 2.4.0"},
       {:calendar, "~> 1.0.0"},
