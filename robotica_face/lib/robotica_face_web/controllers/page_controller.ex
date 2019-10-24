a3045f06bbe6114932bac1c9992ca27c741d8dc5
Brian May <brian@linuxpenguins.xyz>
2019-10-25 08:05:31 +1100

    Split website up into multiple pages

--- robotica_face/lib/robotica_face_web/controllers/page_controller.ex
+++ robotica_face/lib/robotica_face_web/controllers/page_controller.ex
@@ -4,4 +4,16 @@ defmodule RoboticaFaceWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def local(conn, _params) do
+    render(conn, "local.html")
+  end
+
+  def remote(conn, _params) do
+    render(conn, "remote.html")
+  end
+
+  def schedule(conn, _params) do
+    render(conn, "schedule.html")
+  end
 end
--- robotica_face/lib/robotica_face_web/controllers/schedule_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule RoboticaFaceWeb.ScheduleController do
-  use RoboticaFaceWeb, :controller
-
-  def upcoming_list(conn, _params) do
-    render(conn, "schedule.html")
-  end
-end
