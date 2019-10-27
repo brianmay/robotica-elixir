99b2b04e2dea758e4f32e231cfc7480028350e5e
Brian May <brian@linuxpenguins.xyz>
2019-10-27 11:03:45 +1100

    Make Tesla page better

--- robotica_face/lib/robotica_face_web/live/tesla.ex
+++ robotica_face/lib/robotica_face_web/live/tesla.ex
@@ -3,7 +3,71 @@ defmodule RoboticaFaceWeb.Live.Tesla do
 
   def render(assigns) do
     ~L"""
-    <p><%= inspect(@tesla_state, pretty: true) |> Phoenix.HTML.Format.text_to_html %></p>
+    <%= if @tesla_state != %{} do %>
+    <table>
+    <tbody>
+
+    <tr>
+    <th>
+    <td>Battery Charge</td>
+    <td><%= @tesla_state["state"]["battery_level"] %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Charger Plugged In</td>
+    <td><%= @tesla_state["state"]["charger_plugged_in"] %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Is Home</td>
+    <td><%= @tesla_state["state"]["is_home"] %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Unlocked time</td>
+    <td><%= @tesla_state["state"]["unlocked_time"] %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Unlocked Delta</td>
+    <td><%= @tesla_state["state"]["unlocked_delta"] %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Battery Charge Time</td>
+    <td><%= @tesla_state["history"]["battery_charge_time"] * 60 %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Outside Temperature</td>
+    <td><%= @tesla_state["history"]["outside_temp"] %></td>
+    </th>
+    </tr>
+
+    <tr>
+    <th>
+    <td>Inside Temperature</td>
+    <td><%= @tesla_state["history"]["inside_temp"] %></td>
+    </th>
+    </tr>
+
+    </tbody>
+    </table>
+    <% else %>
+    No state date found.
+    <% end %>
     """
   end
 
