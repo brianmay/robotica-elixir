fb337ef4e2c48290727f2b179df4851efb591e2d
Brian May <brian@linuxpenguins.xyz>
2019-11-01 15:34:15 +1100

    Use custom icon for map

--- robotica_face/assets/js/app.js
+++ robotica_face/assets/js/app.js
@@ -21,30 +21,50 @@ import LiveSocket from "phoenix_live_view";
 
 let Hooks = {};
 
+function plane(bearing) {
+  // bearing = -45;
+  return L.icon({
+    iconUrl: `data:image/svg+xml;utf8,<svg viewBox="-5 -5 30 30" xmlns="http://www.w3.org/2000/svg">
+
+   <g transform="rotate(${45 + parseInt(bearing)},10,10)">
+   <rect x="0" y="0" width="20" height="20" stroke="black" stroke-width="0.1" fill="none" />
+   <path stroke="red" stroke-width="1" fill="none" d="M 0,0 L 5,0 M 0,0 L 0,5 M 0,0 L 20,20 M 15, 5 L 5, 15 " />
+   </g>
+
+</svg>`,
+      iconSize: [64, 64],
+      iconAnchor: [32, 32],
+  });
+}
+
 Hooks.Map = {
     mounted() {
         let latitude = parseFloat(this.el.getAttribute("data-latitude"));
         let longitude = parseFloat(this.el.getAttribute("data-longitude"));
+        let heading = parseFloat(this.el.getAttribute("data-heading"));
 
         let map = L.map('mapid');
 
         // create the tile layer with correct attribution
 	      let osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	      let osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-	      let osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
+	      let osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib});
         map.addLayer(osm);
         this.map = map;
 
         map.setView([latitude, longitude], 16);
-        this.marker = L.marker([latitude, longitude]).addTo(map);
+        this.marker = L.marker([latitude, longitude], {
+            icon: plane(heading),
+        }).addTo(map);
     },
 
     updated() {
         let latitude = parseFloat(this.el.getAttribute("data-latitude"));
         let longitude = parseFloat(this.el.getAttribute("data-longitude"));
+        let heading = parseFloat(this.el.getAttribute("data-heading"));
         let map = this.map;
         map.setView([latitude, longitude], 16);
-        this.marker.setLatLng([latitude, longitude]);
+        this.marker.setLatLng([latitude, longitude]).setIcon(plane(heading));
     }
 }
 
