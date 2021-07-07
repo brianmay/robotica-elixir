24da35fa4cedaf4d9a8c503fb286e3e94f763710
Brian May <brian@linuxpenguins.xyz>
2021-07-07 17:24:27 +1000

    Fix formatting.

--- robotica_common/lib/subscriptions.ex
+++ robotica_common/lib/subscriptions.ex
@@ -83,9 +83,7 @@ defmodule RoboticaCommon.Subscriptions do
     case get_message_format(raw_message, format) do
       {:ok, message} ->
         Logger.debug(
-          "Dispatching #{inspect(topic)} #{inspect(raw_message)} #{inspect(format)} #{
-            inspect(message)
-          }."
+          "Dispatching #{inspect(topic)} #{inspect(raw_message)} #{inspect(format)} #{inspect(message)}."
         )
 
         :ok = GenServer.cast(pid, {:mqtt, topic, label, message})
