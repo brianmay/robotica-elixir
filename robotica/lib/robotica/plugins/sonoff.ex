8eb84419e3396aea9b727e56ec1ddc003ed2aa11
Brian May <brian@linuxpenguins.xyz>
2021-10-19 09:02:38 +1100

    Add postfix option to sonoff plugin

--- robotica/lib/robotica/plugins/sonoff.ex
+++ robotica/lib/robotica/plugins/sonoff.ex
@@ -8,8 +8,8 @@ defmodule Robotica.Plugins.SonOff do
 
   defmodule Config do
     @moduledoc false
-    @type t :: %__MODULE__{topic: String.t()}
-    defstruct [:topic]
+    @type t :: %__MODULE__{topic: String.t(), postfix: String.t()}
+    defstruct [:topic, :postfix]
   end
 
   defmodule State do
@@ -46,8 +46,17 @@ defmodule Robotica.Plugins.SonOff do
   ## Server Callbacks
 
   def init(plugin) do
+    state = %State{
+      config: plugin.config,
+      location: plugin.location,
+      device: plugin.device,
+      last_power: nil
+    }
+
+    postfix = get_postfix(state)
+
     Robotica.Subscriptions.subscribe(
-      ["stat", plugin.config.topic, "POWER"],
+      ["stat", plugin.config.topic, "POWER#{postfix}"],
       :power,
       self(),
       :raw,
@@ -62,23 +71,25 @@ defmodule Robotica.Plugins.SonOff do
       :resend
     )
 
-    {:ok,
-     %State{
-       config: plugin.config,
-       location: plugin.location,
-       device: plugin.device,
-       last_power: nil
-     }}
+    {:ok, state}
   end
 
   def config_schema do
     %{
       struct_type: Config,
-      topic: {:string, true}
+      topic: {:string, true},
+      postfix: {:string, false}
     }
   end
 
-  defp handle_command(state, command) do
+  defp get_postfix(%State{} = state) do
+    case state.config.postfix do
+      nil -> ""
+      postfix -> postfix
+    end
+  end
+
+  defp handle_command(%State{} = state, command) do
     publish_command(state.location, state.device, command)
 
     power =
@@ -89,13 +100,14 @@ defmodule Robotica.Plugins.SonOff do
       end
 
     if power != nil do
-      :ok = Robotica.Mqtt.publish_raw("cmnd/#{state.config.topic}/power", power)
+      postfix = get_postfix(state)
+      :ok = Robotica.Mqtt.publish_raw("cmnd/#{state.config.topic}/power#{postfix}", power)
     else
       :ok
     end
   end
 
-  def handle_cast({:mqtt, _, :command, command}, state) do
+  def handle_cast({:mqtt, _, :command, command}, %State{} = state) do
     case Robotica.Config.validate_device_command(command) do
       {:ok, command} ->
         case check_type(command, "device") do
@@ -110,13 +122,13 @@ defmodule Robotica.Plugins.SonOff do
     {:noreply, state}
   end
 
-  def handle_cast({:mqtt, _, :power, power}, state) do
+  def handle_cast({:mqtt, _, :power, power}, %State{} = state) do
     publish_device_state(state, power)
     state = %State{state | last_power: power}
     {:noreply, state}
   end
 
-  def handle_cast({:mqtt, _, :lwt, msg}, state) do
+  def handle_cast({:mqtt, _, :lwt, msg}, %State{} = state) do
     cond do
       msg != "Online" -> publish_device_hard_off(state)
       state.last_power != nil -> publish_device_state(state, state.last_power)
