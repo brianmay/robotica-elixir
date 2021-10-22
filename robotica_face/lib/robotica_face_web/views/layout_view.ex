68f6a80e122e817698e3bb803edd205226c79185
Brian May <brian@linuxpenguins.xyz>
2021-10-23 10:41:42 +1100

    Simplify link classes

--- robotica_face/lib/robotica_face_web/views/layout_view.ex
+++ robotica_face/lib/robotica_face_web/views/layout_view.ex
@@ -1,6 +1,20 @@
 defmodule RoboticaFaceWeb.LayoutView do
   use RoboticaFaceWeb, :view
 
-  def active_class(active, active), do: "active"
-  def active_class(_, _), do: ""
+  @spec prepend_if(list :: list(), condition :: bool(), item :: any()) :: list()
+  defp prepend_if(list, condition, item) do
+    if condition, do: [item | list], else: list
+  end
+
+  def item_class(active, item) do
+    ["nav-item"]
+    |> prepend_if(active == item, "active")
+    |> Enum.join(" ")
+  end
+
+  def link_class(active, item) do
+    ["nav-link"]
+    |> prepend_if(active == item, "active")
+    |> Enum.join(" ")
+  end
 end
