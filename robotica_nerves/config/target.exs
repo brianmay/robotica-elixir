a4c027e0f2f8c3a0d386f4864c888fd364064dd3
Brian May <brian@linuxpenguins.xyz>
2022-06-08 08:11:19 +1000

    Remove obsolete use_system_registry setting

--- robotica_nerves/config/target.exs
+++ robotica_nerves/config/target.exs
@@ -24,12 +24,6 @@ config :shoehorn,
   ],
   app: Mix.Project.config()[:app]
 
-# Nerves Runtime can enumerate hardware devices and send notifications via
-# SystemRegistry. This slows down startup and not many programs make use of
-# this feature.
-
-config :nerves_runtime, :kernel, use_system_registry: false
-
 # Erlinit can be configured without a rootfs_overlay. See
 # https://github.com/nerves-project/erlinit/ for more information on
 # configuring erlinit.
