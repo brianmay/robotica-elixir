b0d66d724193584d4bf229c56f0f91e86014620f
Brian May <brian@linuxpenguins.xyz>
2022-11-11 12:35:27 +1100

    Update Elxir/OTP/nerves

--- robotica_nerves/mix.exs
+++ robotica_nerves/mix.exs
@@ -40,7 +40,7 @@ defmodule RoboticaNerves.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.7.15", runtime: false},
+      {:nerves, "~> 1.9.0", runtime: false},
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.8.3"},
       {:toolshed, "~> 0.2.13"},
@@ -54,15 +54,15 @@ defmodule RoboticaNerves.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.18", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.18", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.18", runtime: false, targets: :rpi2},
-      {:robotica_rpi3, "~> 1.18", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.18", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.18", runtime: false, targets: :rpi4},
-      {:nerves_system_bbb, "~> 2.13", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.9", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.18", runtime: false, targets: :x86_64},
+      {:nerves_system_rpi, "~> 1.21", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.21", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.21", runtime: false, targets: :rpi2},
+      {:robotica_rpi3, "~> 1.21", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.21", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.21", runtime: false, targets: :rpi4},
+      {:nerves_system_bbb, "~> 2.14", runtime: false, targets: :bbb},
+      {:nerves_system_osd32mp1, "~> 0.10", runtime: false, targets: :osd32mp1},
+      {:nerves_system_x86_64, "~> 1.21", runtime: false, targets: :x86_64},
       {:robotica, path: "../robotica"},
       {:robotica_common, path: "../robotica_common"},
       {:robotica_ui, path: "../robotica_ui"},
