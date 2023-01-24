c5914c902a8bc8680d5026f9dacc68c2bf6c5017
Brian May <brian@linuxpenguins.xyz>
2023-01-24 12:36:29 +1100

    Remove docker stuff; not used any more

--- robotica_common/config/docker.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :robotica_common,
-  compile_config_files: false,
-  config_common_file: nil
--- robotica_docker/test/robotica_docker_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RoboticaDockerTest do
-  use ExUnit.Case
-  doctest RoboticaDocker
-
-  test "greets the world" do
-    assert RoboticaDocker.hello() == :world
-  end
-end
